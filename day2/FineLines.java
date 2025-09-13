fa1d8f1d08f4bbb258b55b450b07656d35b28512
Sowmya-sree123 <rachurisowmya6@gmail.com>
2025-09-13 19:47:41 +0530

    Update and rename FineLines.java to day2/FineLines.java

--- day2/FineLines.java
+++ day2/FineLines.java
@@ -1,9 +1,10 @@
 package day_2;
 
-public class FineLines 
+public class FiveLines 
 {
 	public static void main(String[] args) {
 		System.out.println("Line1\nLine2\nLine3\nLine4\nLine5");
 	}
 
 }
+
