fa4661f9f20b132930da6a626688fe881030a3b7
Sowmya-sree123 <rachurisowmya6@gmail.com>
2025-09-13 19:31:45 +0530

    Add files via upload

---
+++ About_Me.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class About_Me 
+{
+	public static void main(String[] args) {
+		System.out.println("Hello,friends");
+		System.out.println("My name is sowmya");
+		System.out.println("I am from Vemulapalli");
+		System.out.println("I am 22 years old");
+		System.out.println("Thankyou for listening this!");
+	}
+
+}
---
+++ Small_Program.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class Small_Program 
+{
+	public static void main(String[] args) {
+		System.out.println("Java is fun!");
+		System.out.println("Java is fun!");
+		System.out.println("Java is fun!");
+		System.out.println("Java is fun!");
+		System.out.println("Java is fun!");
+	}
+
+}
